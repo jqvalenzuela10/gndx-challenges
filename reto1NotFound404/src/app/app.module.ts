ce0578ecc36a5a0fffd19afaef2d9429a3073959
Jonathan Quilca Valenzuela <jqvalenzuela10@gmail.com>
2021-01-13 18:03:31 -0500

    estructurando el proyecto reto 1

--- reto1NotFound404/src/app/app.module.ts
+++ reto1NotFound404/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Page404Component } from './page404/page404.component';
+import { HeaderComponent } from './page404/header/header.component';
+import { BodyComponent } from './page404/body/body.component';
+import { FooterComponent } from './page404/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page404Component,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
